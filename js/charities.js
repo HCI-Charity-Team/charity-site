498401b35596686683567a47a0a9115d722bf3b0
Aura Ulloa <auraulloa@dhcp-10-105-208-57.wireless.northwestern.private>
2020-02-19 16:11:51 -0600

    added changes to css files

--- js/charities.js
+++ js/charities.js
@@ -1,2 +1,50 @@
+const jsonObj = {"total_results":16, "organizations":[
+    {"name":"Alliance of Hope for Suicide Loss Suicide", "img": "https://cdn.greatnonprofits.org/images/logos/lotusnight.jpg", "geo":"", "des":"","category":"mental health"},
+    {"name":"The Cradle", "img": "https://cdn.greatnonprofits.org/images/logos/Cradlelogoblue7705withCG9tagline.jpg", "geo":"", "des":"","category":"education"},
+    {"name":"Fabretto Children's Foundation", "img": "https://cdn.greatnonprofits.org/images/logos/2013FabrettoLogoAPPROVED-01250px.png", "geo":"", "des":"","category":"human services"},
+    {"name":"Rainbows for All Children", "img": "https://cdn.greatnonprofits.org/images/logos/Rainbows4all_logo_COLOR2.jpg", "geo":"", "des":"","category":"human services"},
+    {"name":"Benevolent", "img": "https://cdn.greatnonprofits.org/images/logos/BenevolentFulllogo1.jpg", "geo":"", "des":"","category":"human services"},
+    {"name":"Impact Behavioral Health Partners", "img":"https://cdn.greatnonprofits.org/images/logos/Impact-Primary-Color-EMAIL1.jpg", "geo":"", "des":"","category":"mental health"},
+    {"name":"The Rotary Foundation of Rotary International", "img": "", "geo":"", "des":"","category":"international"},
+    {"name":"Kidney Cancer Association", "img": "https://cdn.greatnonprofits.org/images/logos/itunespodcast300x300.jpg", "geo":"", "des":"","category":"health"},
+    {"name":"Field Ready Inc", "img":"https://cdn.greatnonprofits.org/images/logos/FIELDREADYLogoAIfile.jpg", "geo":"", "des":"","category":"public/societal benefit"},
+    {"name":"YWCA Evanston North Shore", "img": "", "geo":"", "des":"","category":"advocacy"},
+    {"name":"Infant Welfare Society of Evanston, Inc", "img": "", "geo":"", "des":"","category":"human services"},
+    {"name":"Living Earth Television NFP", "img": "https://cdn.greatnonprofits.org/images/logos/LETVcalliglogo.JPG", "geo":"", "des":"","category":"arts/culture"},
+    {"name":"Allowance of Good", "img": "", "geo":"", "des":"","category":"international"},
+    {"name":"Learning Bridge Early Education Center", "img":"", "geo":"", "des":"","category":"education"},
+    {"name":"Evanston Environmental Association", "img": "https://cdn.greatnonprofits.org/images/logos/ecology-center-rendering-w-.jpg", "geo":"", "des":"","category":"environment/animals"},
+    {"name":"Girls Play Sports, Inc", "img": "https://cdn.greatnonprofits.org/images/logos/clubGPS_3c_border0.jpg", "geo":"", "des":"", "category":"education"},
+    {"name":"Youth Job Center Inc", "img":"", "geo":"", "des":"", "category":"other"}   
+]};
+
+let charities = jsonObj["organizations"];
+
+for(charity of charities){
+    if (charity.img === "") {
+        let template = `
+            <div>
+                <h3>${charity.name}</h3>
+                <div>
+                <img src="https://www.stickpng.com/assets/images/5a461402d099a2ad03f9c997.png">
+                </div>
+                <p>${charity.category}</p>
+            </div>
+        `;
+        document.querySelector('section').innerHTML += template;
+    } else {
+        let template = `
+            <div>
+                <h3>${charity.name}</h3>
+                <div>
+                <img src= ${charity.img}>
+                </div>
+                <p>${charity.category}</p>
+            </div>
+        `;
+        document.querySelector('section').innerHTML += template;
+    }
+}
+
 urlParams = new URLSearchParams(window.location.search);
 document.getElementById('searchBox').value = urlParams.get('search')
