f25f824acd859293163dbcd372e37376f5812698
ivyxuan <iloveivyxuan@gmail.com>
2020-02-19 19:30:45 -0600

    delete some console.log

--- js/charities.js
+++ js/charities.js
@@ -1,6 +1,6 @@
-
 urlParams = new URLSearchParams(window.location.search);
 document.getElementById('searchBox').value = urlParams.get('search')
+
 const local_path = './data/';
 const remote_path = 'https://raw.githubusercontent.com/HCI-Charity-Team/charity-site/connor-branch/data/';
 var selected_tag;
@@ -8,19 +8,19 @@ var clist;
 let causes;
 causes = ["all", "animals", 'arts', 'environment', 'education', 'health', 'human services', 'mental health', 'women', 'stem'];
 function filter_charities(charity_list, cause) {
-    
+
     //var charity_list = list_object[0]
     //var cause = list_object[1]
     console.clear();
     for (org of charity_list.organizations) {
-        
+
         if ((org.category === cause) || (cause === 'all')) {
             // DO SOMETHING WITH THE ORGANIZATIONS ONCE THEY'RE FILTERED
-            
+
             console.log("NAME: " + org.name + " CAUSE: " + org.category);
             console.log(cause)
         }
-        
+
         //document.write("<br>");
 
     }
@@ -48,13 +48,8 @@ console.log(clist);
 let idx = 0;
 
 buttons_arr.forEach(function(button, index, array){
-    button.onclick = function() { 
+    button.onclick = function() {
         cause = causes[index];
         filter_charities(clist, cause)
     };
 });
-
-
-//filter_charities(clist, 'all');
-//buttons_arr[0].click();
-
