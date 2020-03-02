efec4b121f641aac6a2ae2b0468975f2c095b0e1
ivyxuan <iloveivyxuan@gmail.com>
2020-03-01 18:45:16 -0600

    Add login page

---
+++ js/firebase/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3CUTJ4E9jLD1rVX8G0LXzrvf6_ZwOZwA",
+  authDomain: "sharity-bd26d.firebaseapp.com",
+  databaseURL: "https://sharity-bd26d.firebaseio.com",
+  projectId: "sharity-bd26d",
+  storageBucket: "sharity-bd26d.appspot.com",
+  messagingSenderId: "344118016032",
+  appId: "1:344118016032:web:a1577dfba7b617689e2445"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
