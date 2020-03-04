dc9eb1783f47c30e9059961cf7f441619a66311a
Aura Ulloa-Ordonez <auraulloa.9000@gmail.com>
2020-03-04 16:33:53 -0600

    Added user profile

--- js/sign_up.js
+++ js/sign_up.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const role = urlParams.get('as');
 
 if (role === 'individual') {
-  document.querySelector('#slogan').innerHTML = 'Discover the nearby charities.';
+  document.querySelector('#slogan').innerHTML = 'Discover nearby charities.';
   document.querySelector('#sloganImage').src = './images/logo_sign_in.png';
   document.querySelector('.auth-sidebar').style.backgroundColor = '#FFEBE9';
   document.querySelector('.auth-sidebar').style.color = '#945A54';
@@ -11,7 +11,7 @@ if (role === 'individual') {
   document.querySelector('.top a').innerHTML = "I'm not an individual. &nbsp Sign in as a charity";
   document.querySelector('.top a').href = 'sign_up.html?as=charity'
 } else {
-  document.querySelector('#slogan').innerHTML = 'Extra donations and helps your charity deserves.';
+  document.querySelector('#slogan').innerHTML = 'The extra donations and help your charity deserves.';
   document.querySelector('#sloganImage').src = './images/logo_for_charity.jpg';
   document.querySelector('.auth-sidebar').style.backgroundColor = '#FBDDD1';
   document.querySelector('.auth-sidebar').style.color = '#764B3A';
