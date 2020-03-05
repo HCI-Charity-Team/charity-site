e2c941e68979798fff1cce20602ddc197b594a05
ivyxuan <iloveivyxuan@gmail.com>
2020-03-04 21:12:10 -0600

    Add user name on header

---
+++ js/firebase/auth.js
@@ -0,0 +1,19 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        document.querySelector('#userName').innerHTML = `Hello, ${displayName}`;
+        if (photoURL) {
+            document.querySelector('#userAvatar').src = photoURL;
+        }
+    } else {
+        // User is signed out.
+        // ...
+    }
+});
