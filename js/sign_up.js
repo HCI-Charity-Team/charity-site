c2572c91b598d6fd8a918f2ef960e5f3b02f6760
ivyxuan <iloveivyxuan@gmail.com>
2020-03-12 22:16:41 -0500

    Fix sign up page

--- js/sign_up.js
+++ js/sign_up.js
@@ -2,21 +2,13 @@ const urlParams = new URLSearchParams(window.location.search);
 const role = urlParams.get('as');
 
 if (role === 'individual') {
-  document.querySelector('#slogan').innerHTML = 'Discover nearby charities.';
-  document.querySelector('#sloganImage').src = './images/logo_sign_in.png';
-  document.querySelector('.auth-sidebar').style.backgroundColor = '#FFEBE9';
-  document.querySelector('.auth-sidebar').style.color = '#945A54';
-
-  document.querySelector('#role').innerHTML = 'Sign in to Sharity as an individual to donate or join a charity.'
+  document.querySelector('video').src = 'https://cdn.dribbble.com/users/14268/screenshots/8921521/media/dc5be48952b04977b562cf8352b22085.mp4';
+  document.querySelector('#role').innerHTML = 'Sign in as an individual to donate or join a charity.'
   document.querySelector('.top a').innerHTML = "I'm not an individual. &nbsp Sign in as a charity";
   document.querySelector('.top a').href = 'sign_up.html?as=charity'
 } else {
-  document.querySelector('#slogan').innerHTML = 'The extra donations and help your charity deserves.';
-  document.querySelector('#sloganImage').src = './images/logo_for_charity.jpg';
-  document.querySelector('.auth-sidebar').style.backgroundColor = '#FBDDD1';
-  document.querySelector('.auth-sidebar').style.color = '#764B3A';
-
-  document.querySelector('#role').innerHTML = 'Sign in to Sharity as a charity to get more help.'
+  document.querySelector('video').src = 'https://cdn.dribbble.com/users/14268/screenshots/8778221/media/eeaa027529e04701008f62bcd3b0cd72.mp4';
+  document.querySelector('#role').innerHTML = 'Sign in as a charity to get more help.'
   document.querySelector('.top a').innerHTML = "I'm not a charity. &nbsp Sign in as an individual";
   document.querySelector('.top a').href = 'sign_up.html?as=individual'
 }
