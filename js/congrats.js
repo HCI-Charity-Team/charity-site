336b69295c72d630ccc6d9c05d91aa91b730b083
Aura Ulloa-Ordonez <auraulloa.9000@gmail.com>
2020-03-14 17:35:26 -0500

    Fixed charity form redirect

--- js/congrats.js
+++ js/congrats.js
@@ -1,7 +1,7 @@
 var timeleft = 6;
 var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
-    window.location.href = 'charity-site/index.html';
+    window.location.href = 'index.html';
   }
   document.querySelector('#time').innerHTML = timeleft;
   timeleft -= 1;
