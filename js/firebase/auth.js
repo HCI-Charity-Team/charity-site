ca212a967a66874cf1a92c8ddb74a9cca7a55446
ivyxuan <iloveivyxuan@gmail.com>
2020-03-14 00:05:03 -0500

    Fix individual profile header

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -10,7 +10,10 @@ firebase.auth().onAuthStateChanged(function(user) {
         const providerData = user.providerData;
 
         const defaultPhotoUrl = "https://img1.wikia.nocookie.net/__cb20140209140937/spudroosi/images/thumb/b/bb/Default_channel_icon.png/500px-Default_channel_icon.png";
-        document.querySelector('#signInBtn').style.display = 'none';
+        if (document.querySelector('#signInBtn')) {
+            document.querySelector('#signInBtn').style.display = 'none';
+        }
+
         const format = `
             <div class="user">
                 <h3 id="userName">Hello, ${displayName}</h3>
@@ -22,7 +25,7 @@ firebase.auth().onAuthStateChanged(function(user) {
                 type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
                     <button class="dropdown-item" type="button">Profile</button>
-                    <button class="dropdown-item" type="button">Log out</button>
+                    <button id="logout" class="dropdown-item" type="button">Log out</button>
                 </div>
             </div>`
         document.querySelector('.header .nav').innerHTML = format;
@@ -33,8 +36,19 @@ firebase.auth().onAuthStateChanged(function(user) {
             document.querySelector('.change_role').style.display = 'none';
         }
 
+        const logout = () => {
+            firebase.auth().signOut()
+                .then(function() {
+
+                })
+                .catch(function(error) {
+
+            });
+        }
+        document.querySelector('#logout').addEventListener('click', logout);
+
     } else {
-        // User is signed out.
-        // ...
+        const format = '<a id="signInBtn" href="sign_up.html?as=individual" class="add-chairty">Sign In</a>';
+        document.querySelector('.header .nav').innerHTML = format;
     }
 });
